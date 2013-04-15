ad11de04b7f6d59e327ff76eee1183fe4408490b
Ethan Laur <phyrrus9@gmail.com>
2013-04-15 12:32:09 -0400

    Initial Commit

---
+++ resolve.c
@@ -0,0 +1,35 @@
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h> 
+#include <signal.h>
+#include <time.h>
+#include <time.h>
+
+#define OFFSET MOFFSET
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+	printf("Missing args\n");
+	return -1;
+    }
+    else
+    {
+	for (int i = 1; i < argc; i++)
+	{
+		struct hostent *hp;
+		hp = gethostbyname2(argv[i], AF_INET);
+		printf("%s: %s\n", argv[i], inet_ntoa(*((struct in_addr *)hp->h_addr)));
+	}
+    }
+
+    return 0;
+}
